2ba63430cb0607d5830e3891745693a87b60e5b8
antonionikolov <antonionaidenovnikolov@abv.bg>
2014-02-22 04:21:02 +0200

    No -1 data

--- progress_and_records/progress.rb
+++ progress_and_records/progress.rb
@@ -9,31 +9,31 @@ class Progress
 
   def squat
     db = SQLite3::Database.new("strength_training.db")
-    period = db.execute("select date, squat from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date }
+    period = db.execute("select date, squat from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date and date[1] != -1 }
     period.last[1] - period.first[1]
   end
 
   def deadlift
     db = SQLite3::Database.new("strength_training.db")
-    period = db.execute("select date, deadlift from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date }
+    period = db.execute("select date, deadlift from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date and date[1] != -1 }
     period.last[1] - period.first[1]
   end
 
   def bench_press
     db = SQLite3::Database.new("strength_training.db")
-    period = db.execute("select date, bench_press from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date }
+    period = db.execute("select date, bench_press from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date and date[1] != -1 }
     period.last[1] - period.first[1]
   end
 
   def shoulder_press
     db = SQLite3::Database.new("strength_training.db")
-    period = db.execute("select date, shoulder_press from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date }
+    period = db.execute("select date, shoulder_press from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date and date[1] != -1 }
     period.last[1] - period.first[1]
   end
 
   def pull_up
     db = SQLite3::Database.new("strength_training.db")
-    period = db.execute("select date, pull_up from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date }
+    period = db.execute("select date, pull_up from #{person.name} order by date").select { |date| from_date <= date[0] and date[0] <= to_date and date[1] != -1 }
     period.last[1] - period.first[1]
   end
 end
